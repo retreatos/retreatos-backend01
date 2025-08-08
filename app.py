c89cb740540d1cf13e5898808e69a8147a8d3dd8
retreatos <admonig1965@gmail.com>
2025-08-08 18:35:32 +0300

    Update app.py

--- app.py
+++ app.py
@@ -61,8 +61,7 @@ def authorize():
     session['email'] = email
     return jsonify(message=f"Logged in as {email}")
 
-# הפעלת האפליקציה
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
